a84c18dce6cb3134af1ec5ed8c1633b07d04c83c
Sahil <sahilbathla1@gmail.com>
2015-01-25 00:01:38 +0530

    use if instead of unless

--- app/controllers/api/v1/search_terms_controller.rb
+++ app/controllers/api/v1/search_terms_controller.rb
@@ -1,11 +1,11 @@
 class Api::V1::SearchTermsController < ApplicationController
 
   def fetch_data
-    unless params[:query].squish.empty?
-      query = params[:query].squish.humanize
-      @businesses = Business.where("name like '#{ query }%' and industry_id IS NOT NULL").limit(6)
-      selected_industries = [0] + @businesses.pluck(:industry_id)
-      @industries = Industry.where("name like '#{ query }%' and id NOT IN (?)", selected_industries).limit(8 - @businesses.length)
+    if params[:query].squish.present?
+      query = params[:query].squish.humanize + '%'
+      @businesses = Business.where("name like ? and industry_id IS NOT NULL", query).limit(6)
+      selected_industries = @businesses.pluck(:industry_id).presence || [0]
+      @industries = Industry.where("name like ? and id NOT IN (?)", query, selected_industries).limit(8 - @businesses.length)
       render json: { results: business_json + industry_json, status: :ok }
     else
       render json: [], status: :unprocessable_entity
