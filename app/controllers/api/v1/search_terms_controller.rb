d2ff06b34e43b54fdc97f29a9678e0bb917a55d4
Sahil <sahilbathla1@gmail.com>
2015-01-24 22:43:39 +0530

    Fix case sensitive issue

--- app/controllers/api/v1/search_terms_controller.rb
+++ app/controllers/api/v1/search_terms_controller.rb
@@ -1,9 +1,10 @@
 class Api::V1::SearchTermsController < ApplicationController
   def fetch_data
     if params[:query]
-      @businesses = Business.where("name like '#{ params[:query] }%' and industry_id IS NOT NULL").limit(6)
+      query = params[:query].squish.downcase
+      @businesses = Business.where("LOWER(name) like '#{ query }%' and industry_id IS NOT NULL").limit(6)
       selected_industries = @businesses.pluck(:industry_id)
-      @industries = Industry.where("name like '#{ params[:query] }%' and id NOT IN (?)", selected_industries).limit(8 - @businesses.count)
+      @industries = Industry.where("LOWER(name) like '#{ query }%' and id NOT IN (?)", selected_industries).limit(8 - @businesses.count)
       render json: { results: business_json + industry_json, status: :ok }
     else
       render json: [], status: :unprocessable_entity
