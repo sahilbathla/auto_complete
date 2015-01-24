ae6243352ed66356201ef225a5942c4187e0387b
Sahil <sahilbathla1@gmail.com>
2015-01-24 22:54:29 +0530

    Fix search squish issue

--- app/controllers/api/v1/search_terms_controller.rb
+++ app/controllers/api/v1/search_terms_controller.rb
@@ -1,10 +1,10 @@
 class Api::V1::SearchTermsController < ApplicationController
   def fetch_data
-    if params[:query]
-      query = params[:query].squish.downcase
-      @businesses = Business.where("LOWER(name) like '#{ query }%' and industry_id IS NOT NULL").limit(6)
+    unless params[:query].squish.empty?
+      query = params[:query].squish.humanize
+      @businesses = Business.where("name like '#{ query }%' and industry_id IS NOT NULL").limit(6)
       selected_industries = @businesses.pluck(:industry_id)
-      @industries = Industry.where("LOWER(name) like '#{ query }%' and id NOT IN (?)", selected_industries).limit(8 - @businesses.count)
+      @industries = Industry.where("name like '#{ query }%' and id NOT IN (?)", selected_industries).limit(8 - @businesses.count)
       render json: { results: business_json + industry_json, status: :ok }
     else
       render json: [], status: :unprocessable_entity
